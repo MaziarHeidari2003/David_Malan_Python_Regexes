f66f8e60657d7053d37e3f43f3af13769a717772
MaziarHeidari2003 <maziarheidari1124@gmail.com>
2023-12-20 19:29:00 -0800

    youtube done

--- watch.py
+++ watch.py
@@ -9,7 +9,7 @@ def main():
 
 def parse(s):
     if re.search(r'^<iframe(.)*><\/iframe>$',s) :
-        url_pattern = re.search(r'(http(s)?:\/\/(www\.)?youtube\.com\/embed\/)([a-zA-Z0-9_]+)',s)
+        url_pattern = re.search(r'(http(s)?:\/\/(www\.)?youtube\.com\/embed\/)([a-zA-Z0-9_]+$)',s)
         if url_pattern:
             split_url = url_pattern.groups()
             return "https://youtu.be/" + split_url[3]  
